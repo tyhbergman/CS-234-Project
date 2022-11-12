1c2ce9257da3efa2acadce90e5e9e21719785ee1
NAnderson3415 <112595363+NAnderson3415@users.noreply.github.com>
2022-11-11 23:43:54 -0500

    Commented out some tester code.

--- TeaShop_NA_TB_LG/src/InputMenu.java
+++ TeaShop_NA_TB_LG/src/InputMenu.java
@@ -185,10 +185,10 @@ public class InputMenu {
 		
 					}
 					System.out.printf("Total Net Cost: $%.2f%n", netCostSum);
-					System.out.println(grossCostTotal);
-					System.out.println(discountAmountTotal);
-					System.out.println(taxAmountTotal);
-					System.out.println(netCostTotal);
+					//System.out.println(grossCostTotal);
+					//System.out.println(discountAmountTotal);
+					//System.out.println(taxAmountTotal);
+					//System.out.println(netCostTotal);
 					//System.out.print("Would you like to export this report? (Y)es | (N)o: ");
 				break;
 				case 3: 
