2ea13058fbfc0e07e340592596cb9e6599b6338e
NAnderson3415 <112595363+NAnderson3415@users.noreply.github.com>
2022-11-12 21:01:57 -0500

    Added in the export methods for the total costs.

--- TeaShop_NA_TB_LG/src/InputMenu.java
+++ TeaShop_NA_TB_LG/src/InputMenu.java
@@ -1,41 +1,74 @@
 
+/**
+Input Menu
+*/
+
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
 import java.util.Map;
-import java.util.Scanner;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.Collection;
+import java.util.Scanner;
+import java.util.Iterator;
+
+/**
+	This class provides a menu to do operations on all of the inputs that the tea shop 
+	purchases. This menu should only be accessible by the manager of the tea shop, since
+	these options pertain to sensitive cost information.
+*/
 
 public class InputMenu {
-	
-	public InputMenu(){
-		
-	}
 
-	public static void inputMenu() {
+	private double totalGrossCost;
+	private double totalTaxAmount;
+	private double totalDiscountAmount;
+	private double totalNetCost;
+	
+	/**
+		Constructs an input menu with the total gross cost of all of the inputs purchased,
+		the total tax amount owed, the total discount amount deducted from the gross cost,
+		and the net total cost that the tea shop will pay for all inputs purchased.
+	*/
+	
+	public InputMenu() {
+	
+		this.totalGrossCost = 0.0;
+		this.totalTaxAmount = 0.0;
+		this.totalDiscountAmount = 0.0;
+		this.totalNetCost = 0.0;
+
+	}
+	
+		public void inputMenu() {
 		
-		Input header = new Input();
+		//Declares an empty constructor to display the line item headers
+		Input header = new Input(); 
+		//Declares a linked list of the inputs to display each line item
 		List<Input> inputDescription = new LinkedList<Input>();
-		Scanner c = new Scanner(System.in);
-		Scanner s = new Scanner(System.in);
+		Scanner c = new Scanner(System.in); //Used for numerical values
+		Scanner s = new Scanner(System.in); //Used for string or char values
 		
 		//Initializing parameter variables
-		int lineItemID = 0;
-		String name = "Generic Vendor";
-		String category = "Generic Category";
-		String input = "Generic Input";
-		double inputCost = 0.0;
-		int inputQuantity = 0;
-		double inputDiscount = 0.00;
-		double taxRate = 0.00;
-		double gc = 0.0;
-		double ta = 0.0;
-		double da = 0.0;
-		double nc = 0.0;
+		int lineItemID = 0; //Sets the line item ID to zero initially
+		String name = "Generic Vendor"; //Initial vendor name
+		String category = "Generic Category"; //Initial vendor category, such as food products
+		String input = "Generic Input"; //Initial input, such as green tea formula
+		double inputCost = 0.0; //Initial input unit cost in $
+		int inputQuantity = 0; //Initial quantity of units purchased
+		double inputDiscount = 0.00; //Initial discount rate applied on the number of inputs purchased
+		double taxRate = 0.00; //Initial tax rate applied on the number of inputs purchased
+		double gc = 0.0; //Initial gross cost total on the quantity of inputs purchased
+		double ta = 0.0; //Initial tax amount total on the quantity of inputs purchased
+		double da = 0.0; //Initial discount amount total derived from the quantity of inputs purchased
+		double nc = 0.0; //Initial net cost total on the quantity of inputs purchased
+		//Constructs a new input, initially populated with the info above
 		Input lineItem = new Input(lineItemID, name, category, input, inputCost, inputQuantity, inputDiscount, taxRate);
-		inputDescription.add(lineItem);
+		inputDescription.add(lineItem); //Adds the line item to the linked list
+		//End initialization
 		
 		//Dummy information that is used to display the initial cost report
 		Input lineItem1 = new Input(1, "Jimmy's", "Tea Products", "Coconut Tea Supplies", 4.00, 60, 0.0, 0.05);
@@ -88,6 +121,7 @@ public class InputMenu {
 		double nc8 = lineItem8.getNetCost(0.05, 1.50, 20, 0.0);
 		//End Dummy information
 		
+		//Map to capture the gross costs of each line item
 		Map<Integer, Double> grossCostTotal = new HashMap<>(); //Declaring map for Total Gross Cost
 		grossCostTotal.put(lineItemID, gc); //Initializing map 
 		//Total Gross Cost calculation with the dummy information from above
@@ -100,6 +134,7 @@ public class InputMenu {
 		grossCostTotal.put(7, gc7);
 		grossCostTotal.put(8, gc8);
 		
+		//Map to capture the tax amount of each line item
 		Map<Integer, Double> taxAmountTotal = new HashMap<>(); //Declaring map for Total Tax Amount
 		taxAmountTotal.put(lineItemID, ta); //Initializing map
 		//Total Tax Amount calculation with the dummy information from above
@@ -112,6 +147,7 @@ public class InputMenu {
 		taxAmountTotal.put(7, ta7);
 		taxAmountTotal.put(8, ta8);
 		
+		//Map to capture the discount amount of each line item
 		Map<Integer, Double> discountAmountTotal = new HashMap<>(); //Declaring map for Total Discount Amount
 		discountAmountTotal.put(lineItemID, da); //Initializing map
 		//Total Discount Amount calculation with the dummy information from above
@@ -124,6 +160,7 @@ public class InputMenu {
 		discountAmountTotal.put(7, da7);
 		discountAmountTotal.put(8, da8);
 		
+		//Map to capture the net cost amount of each line item
 		Map<Integer, Double> netCostTotal = new HashMap<>(); //Declaring map for Total Net Cost Amount
 		netCostTotal.put(lineItemID, nc); //Initializing map
 		//Total Net Cost calculation with the dummy information from above
@@ -135,11 +172,12 @@ public class InputMenu {
 		netCostTotal.put(6, nc6);
 		netCostTotal.put(7, nc7);
 		netCostTotal.put(8, nc8);
-
-		int choice;
+		
+		int choice; //Used for the menu entering options (ie, 1,2,3,etc.)
 		do 
 		{
-		
+			
+			//Input Menu Display
 			System.out.println("----------------------------");
 			System.out.println("|       Inputs Menu        |");
 			System.out.println("----------------------------");
@@ -153,14 +191,14 @@ public class InputMenu {
 			System.out.println("----------------------------");
 			choice = c.nextInt();
 		
-			switch (choice)
+			switch (choice) //Goes through each option the user enters until quit, ie, when 6 is entered.
 			{
 			
-				case 1: 
+				case 1: //Adds a line item of all the information about an input
 					System.out.print("Enter the line item ID for this input (as a nonnegative integer, ie, 0,1,2,18, etc.): ");
 					lineItemID = c.nextInt();
 					boolean exists = false;
-					while (!exists)
+					while (!exists) //Checks if the line item is already existing
 					{
 						if (lineItemID == lineItem.getLineItemID() || lineItemID == lineItem1.getLineItemID() || 
 						lineItemID == lineItem2.getLineItemID() || lineItemID == lineItem3.getLineItemID() ||
@@ -183,9 +221,9 @@ public class InputMenu {
 					}
 					System.out.print("Enter the name of the vendor receiving the input from: ");
 					name = s.nextLine();
-					System.out.print("Enter the category of goods/services the vendor is selling: ");
+					System.out.print("Enter the category of goods/services the vendor is selling, ie, food products, tea products, etc.: ");
 					category = s.nextLine();
-					System.out.print("Enter the particular input that the vendor is providing: ");
+					System.out.print("Enter the particular input that the vendor is providing, ie, green tea formula: ");
 					input = s.nextLine();
 					System.out.print("Enter the unit cost of the input: $");
 					inputCost = c.nextDouble();
@@ -195,19 +233,26 @@ public class InputMenu {
 					inputDiscount = c.nextDouble();
 					System.out.print("Enter the tax rate applied to this input (write as a positive decimal less than 1, ie, 0.05, 0.1, etc.): ");
 					taxRate = c.nextDouble();
+					//Creates a new line item once all the parameters have been entered
 					lineItem = new Input(lineItemID, name, category, input, inputCost, inputQuantity, inputDiscount, taxRate);
+					//Adds the line itme to linked list for displaying
 					inputDescription.add(lineItem);
 					
-					gc = lineItem.getGrossCost(inputCost, inputQuantity);
-					grossCostTotal.put(lineItemID, gc);
-					ta = lineItem.getTaxAmount(taxRate, inputCost, inputQuantity);
-					taxAmountTotal.put(lineItemID, ta);
+					//Calculates the gross cost for the quantity of inputs purchased
+					gc = lineItem.getGrossCost(inputCost, inputQuantity); 
+					grossCostTotal.put(lineItemID, gc); //Adds to the gross cost map
+					//Calculates the tax amount applied for the quantity of inputs purchased
+					ta = lineItem.getTaxAmount(taxRate, inputCost, inputQuantity); 
+					taxAmountTotal.put(lineItemID, ta); //Adds to the tax amount map
+					//Calculates the discount amount applied for the quantity of inputs purchased
 					da = lineItem.getDiscountAmount(inputCost, inputQuantity, inputDiscount);
-					discountAmountTotal.put(lineItemID, da);
+					discountAmountTotal.put(lineItemID, da); //Adds to the discount amount map
+					//Calculates the net cost for the quantity of inputs purchased
 					nc = lineItem.getNetCost(taxRate, inputCost, inputQuantity, inputDiscount);
-					netCostTotal.put(lineItemID, nc);
+					netCostTotal.put(lineItemID, nc); //Adds to the net cost map
 				break;
 				case 2:
+					//Input Report Header
 					System.out.println("-------------------------------------------------");
 					System.out.println("|                Inputs Report                  |");
 					System.out.println("-------------------------------------------------");
@@ -221,6 +266,7 @@ public class InputMenu {
 					System.out.print("-------------------------------------------------");
 					System.out.print("-------------------------\n");
 					
+					//Creates an iterator to go through the linked list to display all the line items
 					Iterator<Input> i = inputDescription.iterator();
 					while(i.hasNext()) 
 					{
@@ -230,7 +276,8 @@ public class InputMenu {
 					
 					}
 					System.out.println("-------------------------------------------------");
-
+					//Sums up all the values in the gross cost map to get the total gross cost
+					//for all inputs purchased
 					Set<Integer> keySet1 = grossCostTotal.keySet();
 					double grossCostSum = 0.0;
 					for (int key : keySet1)
@@ -240,8 +287,10 @@ public class InputMenu {
 						grossCostSum += value;
 		
 					}
+					this.totalGrossCost = grossCostSum;
 					System.out.printf("Total Gross Cost: $%.2f%n", grossCostSum);
-					
+					//Sums up all the values in the discount amount map to get the total
+					//discount amount for all inputs purchased
 					Set<Integer> keySet2 = discountAmountTotal.keySet();
 					double discountAmountSum = 0.0;
 					for (int key : keySet2)
@@ -251,8 +300,10 @@ public class InputMenu {
 						discountAmountSum += value;
 		
 					}
+					this.totalDiscountAmount = discountAmountSum;
 					System.out.printf("Total Discount Amount: $%.2f%n", discountAmountSum);
-					
+					//Sums up all the values in the tax amount map to get the total discount
+					//amount for all inputs purchased
 					Set<Integer> keySet3 = taxAmountTotal.keySet();
 					double taxAmountSum = 0.0;
 					for (int key : keySet3)
@@ -262,8 +313,10 @@ public class InputMenu {
 						taxAmountSum += value;
 		
 					}
+					this.totalTaxAmount = taxAmountSum;
 					System.out.printf("Total Tax Amount: $%.2f%n", taxAmountSum);
-					
+					//Sums up all the values in the net cost amount map to get the total 
+					//net cost amount for all inputs purchased
 					Set<Integer> keySet4 = netCostTotal.keySet();
 					double netCostSum = 0.0;
 					for (int key : keySet4)
@@ -273,13 +326,43 @@ public class InputMenu {
 						netCostSum += value;
 		
 					}
+					this.totalNetCost = netCostSum;
+					//Asks the user if they want to export the total costs as a report
 					System.out.printf("Total Net Cost: $%.2f%n", netCostSum);
-					//System.out.println(grossCostTotal);
-					//System.out.println(discountAmountTotal);
-					//System.out.println(taxAmountTotal);
-					//System.out.println(netCostTotal);
+					System.out.print("Would you like to export the cost report and exit the input menu? (Y)es | (N)o: ");
+					char export = s.next().charAt(0);
+					s.nextLine();
+					boolean done = false;
+					while (!done)
+					{
+					
+					if (export == 'Y' || export == 'y')
+					{
+					
+						displayCostTotals();
+						return;
+					
+					}
+					else if (export == 'N' || export == 'n')
+					{
+					
+						System.out.println("Please continue navigating the menu.");
+						done = true; 
+					
+					}
+					else
+					{
+					
+						System.out.println("Not a valid option. Please try again.");
+						System.out.println("Would you like to export the cost report? (Y)es | (N)o: ");
+						export = s.next().charAt(0);
+						s.nextLine();
+					
+					}		
+					}			
 				break;
 				case 3: 
+					//Search for a particular existing line item
 					boolean found = false;
 					System.out.print("Enter the line item ID of the line item to search for: ");
 					int id = c.nextInt();
@@ -293,6 +376,7 @@ public class InputMenu {
 					System.out.print("-------------------------------------------------");
 					System.out.print("-------------------------\n");
 				    i = inputDescription.iterator();
+				    //Checks if the line item is in the linked list
 					while(i.hasNext()) 
 					{
 					
@@ -315,6 +399,7 @@ public class InputMenu {
 					System.out.println("-------------------------------------------------");
 				break;
 				case 4: 
+					//Deletes a line item and updates the total cost amounts
 					found = false;
 					System.out.print("Enter the line item ID of the line item to search for: ");
 					id = c.nextInt();
@@ -325,6 +410,11 @@ public class InputMenu {
 						Input v = i.next();
 						if (v.getLineItemID() == id)
 						{
+							
+							this.totalGrossCost = totalGrossCost - grossCostTotal.get(v.getLineItemID());
+							this.totalTaxAmount = totalTaxAmount - taxAmountTotal.get(v.getLineItemID());
+							this.totalDiscountAmount = totalDiscountAmount - discountAmountTotal.get(v.getLineItemID());
+							this.totalNetCost = totalNetCost - netCostTotal.get(v.getLineItemID());
 							
 							i.remove();
 							grossCostTotal.remove(v.getLineItemID());
@@ -354,10 +444,11 @@ public class InputMenu {
 					}
 				break;
 				case 5: 
+					//Updates the line item that the user wants to change
 					found = false;
 					System.out.print("Enter the line item ID of the line item to update: ");
 					id = c.nextInt();
-				    	ListIterator<Input> descriptionIter = inputDescription.listIterator();
+				    ListIterator<Input> descriptionIter = inputDescription.listIterator();
 					while(descriptionIter.hasNext()) 
 					{
 					
@@ -397,6 +488,12 @@ public class InputMenu {
 							taxAmountTotal.replace(v.getLineItemID(), ta);
 							discountAmountTotal.replace(v.getLineItemID(), da);
 							netCostTotal.replace(v.getLineItemID(), nc);
+							//
+							this.totalGrossCost = gc;
+							this.totalTaxAmount = ta;
+							this.totalDiscountAmount = da;
+							this.totalNetCost = nc;
+							//
 							found = true;
 							
 						}
@@ -421,12 +518,12 @@ public class InputMenu {
 					}
 				break;
 				default:
-						if (choice != 6)
-						{
+					if (choice != 6) //Return to main menu
+					{
 						
-							System.out.println("Not a valid choice. Please try again.");
+						System.out.println("Not a valid choice. Please try again.");
 						
-						}
+					}
 				break;
 				
 			}
@@ -434,5 +531,69 @@ public class InputMenu {
 		}while (choice != 6); return;
 	
 	}
+
+	/**
+		Report to display the total cost totals when the user wants to export the report.
+		@param none
+		@return non
+	*/
+
+	public void displayCostTotals() {
+		
+		System.out.println("-----------------------------");
+		System.out.println("Cost Totals");
+		System.out.println("-----------------------------");
+		System.out.printf("Total Gross Cost: $%.2f%n", totalGrossCost);
+		System.out.printf("Total Discount Amount: $%.2f%n", totalDiscountAmount);
+		System.out.printf("Total Tax Amount: $%.2f%n", totalTaxAmount);
+		System.out.printf("Total Net Cost: $%.2f%n", totalNetCost);
+		System.out.println("-----------------------------");
+
+	}
 	
+	/**
+		Gets the total gross cost of all the inputs purchased
+		@return the total gross cost
+	*/
+	
+	
+	public double getTotalGrossCost() {
+	
+		return totalGrossCost;
+	
+	}
+	
+	/**	
+		Gets the total tax amount of all the inputs purchased
+		@return the total tax amount
+	*/
+	
+	public double getTotalTaxAmount() {
+	
+		return totalTaxAmount;
+	
+	}
+	
+	/**
+		Gets the total discount amount applied to all of the inputs purchased
+		@return the total discount amount
+	*/
+	
+	public double getTotalDiscountAmount() {
+	
+		return totalDiscountAmount;
+	
+	}
+	
+	/**
+		Gets the total net cost of all the inputs purchased
+		@return the total net cost
+	*/
+	
+	public double getTotalNetCost() {
+	
+		return totalNetCost;
+	
+	}
+
 }
